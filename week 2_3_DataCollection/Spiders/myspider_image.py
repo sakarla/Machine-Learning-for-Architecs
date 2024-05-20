767f60298a5584347993d6abae724002bbecdb57
Mobina <44108288+mobinanoorany@users.noreply.github.com>
2024-05-20 14:10:03 -0400

    Add files via upload

---
+++ week 2_3_DataCollection/Spiders/myspider_image.py
@@ -0,0 +1,24 @@
+#The spider is designed to extract image URLs from a specific web page.
+
+import scrapy
+
+class ArchDailyImagesSpider(scrapy.Spider):
+    # Define the name of the spider
+    name = 'archdaily_images'
+    
+    # Specify the starting URL(s) for the spider
+    start_urls = ['https://www.archdaily.com/882553/luminous-transparent-mixed-use-tower-will-mark-the-skyline-of-saint-malo?ad_medium=widget&ad_name=category-apartments-article-show']
+
+    #The parse method is a mandatory function in Scrapy spiders. It's called with the web page's response as an argument.
+    def parse(self, response):
+        # Extract image URLs from the page using XPath
+        
+        # XPath is used to select all <img> elements on the page, and their 'src' attributes (image URLs) are extracted. 
+        image_urls = response.xpath('//img/@src').extract()
+
+        # Iterate over the extracted image URLs
+        for img_url in image_urls:
+            # Yield each image URL as a dictionary
+            yield {
+                'image_url': img_url,
+            }
